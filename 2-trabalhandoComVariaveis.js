515ded314f8ebd3bd2080b7df0031f6a296a7697
EvandroKaibara <yujievandro@gmail.com>
2024-12-02 16:59:07 -0300

    Aulas do curso

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,11 @@
+console.log("Trabalhando com variaveis");
+//Js é case sensitive
+
+const idade = 29;
+const Idade = 30;
+
+console.log(Idade);
+console.log(idade);
+
+//não fazer
+//mes = "Janeiro";
---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,20 @@
+console.log("Trabalhando com atribuição de variaveis");
+//Js é case sensitive
+
+//const é uma variavel fixa
+//lett é uma variavel que altera
+let primeiroNome = "Evandro";
+const sobrenome = "Kaibara";
+
+//Sempre tentar manter uma Const e nao um Let
+
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //Declarando uma variavel
+idade = 20; //Atribuindo valor
+idade = idade+1;
+console.log(idade)
